395259b78fcddcbd1753c0a06e16b3a45aa385a2
David Liu <david.yx.liu@oracle.com>
2022-01-03 12:28:37 +0800

    Update mysql.sh

--- integration/mysql.sh
+++ integration/mysql.sh
@@ -2,8 +2,8 @@ set -e
 
 install() {
 
-    wget https://objectstorage.ap-seoul-1.oraclecloud.com/n/cn9yc2hk0gzg/b/MySQL/o/mysql-connector-java-8.0.26.jar
-    cp mysql-connector-java-8.0.26.jar oic_conn_agent_installer/agenthome/thirdparty/lib/
-    rm mysql-connector-java-8.0.26.jar
+    wget https://objectstorage.ap-seoul-1.oraclecloud.com/n/cn9yc2hk0gzg/b/MySQL/o/mysql-connector-java-latest.jar
+    cp mysql-connector-java-latest.jar oic_conn_agent_installer/agenthome/thirdparty/lib/
+    rm mysql-connector-java-latest.jar
 }
 $1
