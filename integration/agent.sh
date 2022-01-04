12f62d073914d6b1cbb1c63d29eaf10fa65afadd
DavidLiu <david.yx.liu@oracle.com>
2022-01-04 08:49:46 +0800

    chmod

--- integration/agent.sh
+++ integration/agent.sh
@@ -5,7 +5,7 @@ install() {
     
     wget https://objectstorage.ap-seoul-1.oraclecloud.com/n/cn9yc2hk0gzg/b/oci-material/o/oic_conn_agent_installer.zip
     unzip oic_conn_agent_installer.zip -d oic_conn_agent_installer
-    rm oic_conn_agent_installer.zip jdk-11_linux-x64_bin.rpm
+    rm oic_conn_agent_installer.zip
     # Modify property `oic_URL` and `agent_GROUP_IDENTIFIER`
     vi oic_conn_agent_installer/InstallerProfile.cfg
 
