ee74b89dfafd28f8494fb4a9e6e4d38884b9fb9f
David Liu <david.yx.liu@oracle.com>
2022-10-06 23:19:30 +0800

    Update oas.sh

--- analytic/OAS/oas.sh
+++ analytic/OAS/oas.sh
@@ -8,7 +8,7 @@ cleanup(){
   sudo yum install -y libgfortran # Enable automated machine learning features
 }
 domain-status(){
-  sudo su oracle
+  
   cat /var/log/oas_cloudinit.log
   cat /var/log/oas_create_domain.log
 }
