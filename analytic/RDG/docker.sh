8586339024845110d6909c4cc8e0f02f0231a2af
David Liu <david.yx.liu@oracle.com>
2022-11-13 03:51:48 +0000

    [TODO] Test Maria connection

--- analytic/RDG/docker.sh
+++ analytic/RDG/docker.sh
@@ -7,7 +7,15 @@ run() {
     docker run -d -p=${hostPort}:8080 --name=analytic-rdg rdg:latest $@
     echo "Please make sure your hostPost ${hostPort} is opened in host firewall..."
     echo "URL: http://${host}:${hostPort}/obiee/config.jsp"
-
+}
+run-jdbc() {
+    hostPort=80
+    local driverJar=$1
+    local filename=$(basename $driverJar)
+    run --volume=$driverJar:/home/opc/Oracle/Middleware/Oracle_Home/domain/jettybase/thirdpartyDrivers/$filename
+}
+run-mariaDB() {
+    run-jdbc ./drivers/mariadb-java-client-3.0.9.jar
 }
 tail-log() {
     docker logs -f analytic-rdg
