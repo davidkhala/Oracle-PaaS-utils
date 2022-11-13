8ae967558a1c08990d0d972d0fe32aac92d88ebb
David Liu <david.yx.liu@oracle.com>
2022-11-13 14:28:05 +0000

    [DONE]

--- analytic/RDG/docker.sh
+++ analytic/RDG/docker.sh
@@ -1,24 +1,38 @@
+set -e
+Name=${Name:-analytic-rdg}
+image=${image:-rdg}
+USERNAME=opc
+Oracle_Home=/home/${USERNAME}/RDG
 build() {
-    docker build -t rdg:latest .
+    
+    docker build -t rdg:latest --progress=plain --no-cache . 
 }
 run() {
     local hostPort=${hostPort:-8080}
     local host=${host:-localhost}
-    docker run -d -p=${hostPort}:8080 --name=analytic-rdg rdg:latest $@
+    docker run -d -p=${hostPort}:8080 --name=$Name $@ $image:latest 
     echo "Please make sure your hostPost ${hostPort} is opened in host firewall..."
     echo "URL: http://${host}:${hostPort}/obiee/config.jsp"
 }
 run-jdbc() {
-    hostPort=80
     local driverJar=$1
     local filename=$(basename $driverJar)
-    run --volume=$driverJar:/home/opc/Oracle/Middleware/Oracle_Home/domain/jettybase/thirdpartyDrivers/$filename
+    
+    run --volume=$driverJar:/home/opc/RDG/domain/jettybase/thirdpartyDrivers/$filename
+
 }
 run-mariaDB() {
-    run-jdbc ./drivers/mariadb-java-client-3.0.9.jar
-}
-tail-log() {
-    docker logs -f analytic-rdg
+    run-jdbc $PWD/drivers/mariadb-java-client-3.0.9.jar
 }
 
+_test(){
+    host=168.138.163.237
+    set +e
+    docker stop $Name
+    set -e
+    docker system prune --force
+    build
+    run-mariaDB
+    docker logs -f analytic-rdg
+}
 $@
