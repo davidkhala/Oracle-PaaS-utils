2612674cb0bc3d2048a62426004b39b7a1ebcd68
David Liu <david.yx.liu@oracle.com>
2022-11-13 02:16:16 +0000

    add portmap

--- analytic/RDG/docker.sh
+++ analytic/RDG/docker.sh
@@ -2,7 +2,9 @@ build() {
     docker build -t rdg:latest .
 }
 run() {
-    docker run -d --name=analytic-rdg rdg:latest $@
+    local hostPort=${hostPort:-8080}
+    docker run -d -p=${hostPort}:8080 --name=analytic-rdg rdg:latest $@
+    echo "Please make sure your hostPost ${hostPort} is opened..."
     docker logs -f analytic-rdg
 }
 
